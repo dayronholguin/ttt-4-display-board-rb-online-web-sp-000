2798f0715a495bf97bf901305da528e17c030e30
Dayron Holguin <dayronholguin@yahoo.com>
2019-09-05 20:09:14 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,9 +4,7 @@ def display_board
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
-def board
-  puts board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
 end
 
-display_board(board)
+display_board
 
