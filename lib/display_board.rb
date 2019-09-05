e3ff7eee48d8459114e69e2d0fe150c5d2ef9beb
Dayron Holguin <dayronholguin@yahoo.com>
2019-09-05 21:11:26 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,1 @@
-def display_board(x_wins)
-  puts " x | x | x "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-end
-
-display_board
-
-
+def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
